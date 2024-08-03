a0baf0f759276475f63a2e79cbb180f9c538d7cc
RyuvicSirville <rsogbimal9@gmail.com>
2024-08-03 20:03:17 +0530

    fgnp

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,10 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  
+  build: {
+    chunkSizeWarningLimit: 1000, // Increase the limit from 500kB to 1000kB
+  },
   base: '/CarbonCreditTracker/',
 
   plugins: [react()],
